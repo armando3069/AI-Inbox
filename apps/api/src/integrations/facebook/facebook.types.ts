f9eda38544925726da1d209f66db580c64bc1724
armando3069 <armandoburuian1234@gmail.com>
2026-04-03 04:35:08 +0300

    feat: add facebook types

---
+++ apps/api/src/integrations/facebook/facebook.types.ts
@@ -0,0 +1,12 @@
+export interface FacebookPageOption {
+  pageId: string;
+  pageName: string;
+  pageAccessToken: string;
+  category: string | null;
+}
+
+export interface FacebookPageView {
+  pageId: string;
+  pageName: string;
+  category: string | null;
+}
